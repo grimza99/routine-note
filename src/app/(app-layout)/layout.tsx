a50bebe5d4b25b3ad3a34c537a872a27ea16924d
grimza99 <gbtmxlf@naver.com>
2026-02-04 00:57:28 +0900

    feat: 바닥글 컴포넌트 추가 및 정책 관련 데이터 통합

--- src/app/(app-layout)/layout.tsx
+++ src/app/(app-layout)/layout.tsx
@@ -1,4 +1,4 @@
-import { Header } from '@/widgets';
+import { Footer, Header } from '@/widgets';
 import type { Metadata } from 'next';
 
 export default function RootLayout({
@@ -9,10 +9,10 @@ export default function RootLayout({
   return (
     <>
       <Header />
-
       <main className="flex justify-center">
         <div className="px-4 py-10 md:px-5 md:py-15 lg:px-8 lg:py-20 w-full max-w-250">{children}</div>
       </main>
+      <Footer />
     </>
   );
 }

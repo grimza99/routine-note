2c406d175f4cc9590abe243a3f03af59bdf1060e
grimza99 <gbtmxlf@naver.com>
2026-02-03 23:25:29 +0900

    feat: 마이페이지에 운동 데이터 요약 카드 추가 및 프로필 컴포넌트 스타일 수정

--- src/app/(app-layout)/mypage/page.tsx
+++ src/app/(app-layout)/mypage/page.tsx
@@ -1,9 +1,39 @@
 import MyPageProfile from '@/features/auth/ui/MyPageProfile';
+import { SummaryCard } from '@/shared';
 
 export default function MyPage() {
+  const summaryData = [
+    {
+      title: '총 루틴',
+      iconSrc: '/icons/goal.svg',
+      value: 2 + '개',
+    },
+    {
+      title: '이번달 운동 일수',
+      iconSrc: '/icons/flame.svg',
+      value: 0 + '일',
+    },
+    {
+      title: '챌린지',
+      iconSrc: '/icons/goal.svg',
+      value: 0 + '등',
+    },
+  ];
   return (
     <div>
       <MyPageProfile nickname="닉네임 테스트" imageUrl="jkj" workoutDays={20} />
+      <section className="flex flex-wrap gap-4">
+        {summaryData.map((data) => (
+          <SummaryCard
+            key={data.title}
+            title={data.title}
+            iconSrc={data.iconSrc}
+            value={data.value}
+            variant="secondary"
+            className="flex-1"
+          />
+        ))}
+      </section>
     </div>
   );
 }
--- src/features/auth/ui/MyPageProfile.tsx
+++ src/features/auth/ui/MyPageProfile.tsx
@@ -5,7 +5,7 @@ interface MyPageProfileProps {
 }
 export default function MyPageProfile({ imageUrl, nickname, workoutDays }: MyPageProfileProps) {
   return (
-    <section className="flex flex-col lg:flex-row items-center mt-10 mb-8 border-2 border-border rounded-lg py-6 gap-3">
+    <section className="flex flex-col lg:flex-row items-center border-2 border-border rounded-lg py-6 gap-3">
       <img src={imageUrl || ''} className="w-20 h-20 rounded-full border-2 border-primary object-cover object-center" />
       <div className="flex flex-col items-center lg:items-start space-y-2">
         <span className="text-2xl font-bold">{nickname}</span>

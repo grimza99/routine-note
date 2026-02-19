c9fb537ed64e41ad0aea53994364618418fcbe88
grimza99 <gbtmxlf@naver.com>
2026-02-19 18:16:37 +0900

    feat: UI 개선 및 버튼, 입력 필드 컴포넌트 수정

--- src/app/(auth-layout)/layout.tsx
+++ src/app/(auth-layout)/layout.tsx
@@ -7,13 +7,13 @@ export default function AuthLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="flex h-full flex-col items-center justify-center bg-gray-100 px-4 py-10 md:px-5 md:py-15 lg:px-8 lg:py-20 w-full gap-5">
+    <main className="flex h-full flex-col items-center justify-center px-4 py-10 md:px-5 md:py-15 lg:px-8 lg:py-20 w-full gap-5">
       <Link
         href="/"
-        className="rounded-xl bg-primary text-white px-6 py-4 flex items-center justify-center shadow-sm w-fit"
+        className="rounded-xl bg-primary text-white p-3 md:px-6 md:py-4 flex items-center justify-center shadow-sm w-fit"
       >
-        <h1 className="text-3xl font-bold flex items-center gap-3">
-          <img src="/icons/bolt.white.svg" alt="logo-icon" className="h-10 w-10" />
+        <h1 className="text-2xl font-bold flex items-center gap-3">
+          <img src="/icons/bolt.white.svg" alt="logo-icon" className="h-6 w-6 md:h-8 md:w-8" />
           {PROJECT.NAME}
         </h1>
       </Link>

39a14bd96a24a941f60d4dfdc129b62c5dba168d
grimza99 <gbtmxlf@naver.com>
2026-02-19 19:08:31 +0900

    feat: 페이지 헤더 배너, footer 텍스트 및 스타일 업데이트

--- src/widgets/footer/Footer.tsx
+++ src/widgets/footer/Footer.tsx
@@ -13,14 +13,14 @@ export default function Footer() {
   const modalData = useMemo(() => (activePolicy ? POLICIES[activePolicy] : null), [activePolicy]);
 
   return (
-    <footer className="bg-gray900 text-white">
-      <div className="mx-auto flex w-full max-w-300 flex-col items-center gap-10 px-6 py-15 md:py-20 ">
+    <footer className="bg-gray900 text-text-secondary">
+      <div className="mx-auto flex w-full max-w-300 flex-col items-center gap-10 px-6 py-10 md:py-15 ">
         <div className="flex md:justify-between w-full flex-col md:flex-row gap-6 items-center">
-          <h1 className="text-2xl font-bold flex items-center gap-3">
-            <img src="/icons/bolt.white.svg" alt="logo-icon" className="h-7 w-7" />
+          <h1 className="text-xl text-white md:text-2xl font-bold flex items-center gap-3">
+            <img src="/icons/bolt.white.svg" alt="logo-icon" className="w-5 h-5 md:h-7 md:w-7" />
             {PROJECT.NAME}
           </h1>
-          <div className="flex flex-wrap gap-3 text-sm font-semibold text-text-secondary">
+          <div className="flex flex-wrap gap-3 text-sm font-semibold ">
             <button type="button" onClick={() => setActivePolicy('terms')}>
               이용약관
             </button>
@@ -35,12 +35,12 @@ export default function Footer() {
             </button>
           </div>
         </div>
-        <div className="text-sm text-text-secondary">© 2026 {PROJECT.NAME}. All rights reserved.</div>
+        <div className="text-sm ">© 2026 {PROJECT.NAME}. All rights reserved.</div>
       </div>
 
       <Modal modalId="footer-policy-modal" isOpen={Boolean(modalData)} onClose={() => setActivePolicy(null)}>
         <div className="flex flex-col gap-4 px-6 py-8">
-          <h3 className="text-lg font-semibold text-text-primary">{modalData?.title}</h3>
+          <h3 className="text-2xl font-semibold text-text-primary">{modalData?.title}</h3>
           <ul className="list-disc space-y-2 text-sm text-text-secondary">
             {modalData?.sections.map((section) => (
               <div key={`${modalData.title}-${section.title}`}>

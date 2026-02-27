27bdfd2d839ac97a10d50e931792a76f4e2f31ad
grimza99 <gbtmxlf@naver.com>
2026-02-27 17:22:35 +0900

    feat: 리포트 페이지에 탭 및 월 변경 기능 추가

--- src/app/(web-view-layout)/m/report/page.tsx
+++ src/app/(web-view-layout)/m/report/page.tsx
@@ -1,10 +1,12 @@
 'use client';
-import { useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
 
 import { MonthReport, PrevMonthsReports } from '@/entities';
 import { useMonth } from '@/shared/hooks';
 import { Tabs } from '@/shared/ui';
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
+import { formatDateYearMonth } from '@/shared/libs';
 
 const tabItems = [
   { id: 'current', label: '이번 달 리포트' },
@@ -12,30 +14,80 @@ const tabItems = [
 ];
 
 export default function ReportPage() {
-  const [activeTab, setActiveTab] = useState<'prev' | 'current'>('current');
-  const { currentMonth, monthLabel, handlePrevMonth, handleNextMonth } = useMonth();
-
   return (
-    <div className="flex flex-col gap-5 md:gap-20">
-      <div>
-        <Tabs items={tabItems} activeId={activeTab} onChange={(id) => setActiveTab(id as 'prev' | 'current')} />
-        {activeTab === 'current' && (
-          <div className="flex items-center gap-4 border-2 border-border justify-center py-2 rounded-lg mt-5">
-            <button type="button" onClick={handlePrevMonth} aria-label="이전 달">
-              <ChevronLeftIcon className="size-6 text-primary" />
-            </button>
-            <span className="font-semibold text-xl md:text-3xl text-primary">{monthLabel}</span>
-            <button type="button" onClick={() => handleNextMonth(true)} aria-label="다음 달">
-              <ChevronRightIcon className="size-6 text-primary" />
-            </button>
-          </div>
-        )}
-      </div>
+    <Suspense fallback={null}>
+      <ReportPageContent />
+    </Suspense>
+  );
+}
+
+function ReportPageContent() {
+  const [activeTab, setActiveTab] = useState<'prev' | 'current'>('current');
+  const [month, setMonth] = useState<Date | null>(null);
+  const { currentMonth, monthLabel, handlePrevMonth, handleNextMonth } = useMonth(month);
+
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const tab = searchParams.get('tab');
+    setActiveTab(tab === 'prev' ? 'prev' : 'current');
+    const reportMonth = searchParams.get('report-month');
+    if (reportMonth && tab === 'current') {
+      setMonth(new Date(reportMonth));
+    }
+  }, [searchParams]);
+
+  const handleChangeTab = (id: string) => {
+    const nextTab = id === 'prev' ? 'prev' : 'current';
+    setActiveTab(nextTab);
+
+    const nextParams = new URLSearchParams(searchParams.toString());
+    nextParams.set('tab', nextTab);
+    router.replace(`${pathname}?${nextParams.toString()}`, { scroll: false });
+  };
+
+  const handleChageMonth = (direction: 'prev' | 'next', isNextMonthBlock?: boolean) => {
+    const nextParams = new URLSearchParams(searchParams.toString());
+    let targetMonth = currentMonth;
+
+    if (direction === 'prev') {
+      handlePrevMonth();
+      targetMonth = new Date(currentMonth);
+      targetMonth.setMonth(targetMonth.getMonth() - 1);
+    }
+    if (direction === 'next') {
+      handleNextMonth(isNextMonthBlock);
+      targetMonth = new Date(currentMonth);
+      targetMonth.setMonth(targetMonth.getMonth() + 1);
+    }
+    nextParams.set('report-month', formatDateYearMonth(targetMonth));
+    router.replace(`${pathname}?${nextParams.toString()}`, { scroll: false });
+  };
+  return (
+    <div className="flex flex-col gap-5 md:gap-10">
+      <Tabs items={tabItems} activeId={activeTab} onChange={handleChangeTab} className="border-2" />
       {activeTab === 'current' ? (
-        <MonthReport intialMonth={currentMonth} />
+        <>
+          <div className="flex items-center gap-4 border-2 border-border justify-center py-2 rounded-lg bg-white">
+            <ChevronLeftIcon
+              className="size-6 md:size-9 text-primary"
+              onClick={() => handleChageMonth('prev')}
+              aria-label="이전 달"
+            />
+            <span className="font-semibold text-xl md:text-3xl text-primary">{monthLabel}</span>
+            <ChevronRightIcon
+              className="size-6 md:size-9 text-primary"
+              onClick={() => handleChageMonth('next', true)}
+              aria-label="다음 달"
+            />
+          </div>
+          <MonthReport intialMonth={currentMonth} />
+        </>
       ) : (
         <div className="flex flex-col gap-8 lg:gap-15 items-center">
-          <h2 className="text-xl md:text-3xl font-bold">지난 리포트</h2>
+          <h2 className="text-2xl italic md:text-3xl font-bold">지난 리포트</h2>
           <PrevMonthsReports />
         </div>
       )}
